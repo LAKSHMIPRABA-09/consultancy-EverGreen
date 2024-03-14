ba0c25ffcdacde3693b7af72e0efa9fc25d97989
LAKSHMIPRABA-09 <lakshmiprabas.21it@kongu.edu>
2024-03-14 21:17:47 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+
+    </div>
+  )
+}
